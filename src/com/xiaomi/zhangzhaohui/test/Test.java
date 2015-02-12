50b3f0e20f3d6a792027ad23f2cddac6d466644f
ZhangZhaohui [张朝辉] <zhangzhaohui@xiaomi.com>
2015-02-12 20:12:52 +0800

    add Main function

---
+++ src/com/xiaomi/zhangzhaohui/test/Test.java
@@ -0,0 +1,14 @@
+package com.xiaomi.zhangzhaohui.test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+public class Test {
+    public static void main(String[] args) {
+        List<Long> miids = new ArrayList<Long>();
+        miids.add(45497343L);
+        System.out.println(UUID.randomUUID().toString());
+    }
+}
